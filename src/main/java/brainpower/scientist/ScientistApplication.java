c544e3a187beaed21a1e1d62651828815ad0e25b
Christa Stephens <ChristaStephens@outlook.com>
2018-11-29 16:32:14 -0500

    Initial commit

---
+++ src/main/java/brainpower/scientist/ScientistApplication.java
@@ -0,0 +1,12 @@
+package brainpower.scientist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScientistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScientistApplication.class, args);
+	}
+}
---
+++ src/test/java/brainpower/scientist/ScientistApplicationTests.java
@@ -0,0 +1,16 @@
+package brainpower.scientist;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ScientistApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
