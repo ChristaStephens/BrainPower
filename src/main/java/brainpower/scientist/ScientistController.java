e468e17065369aa911171e0b0a88e79132c2d47f
Sophia Sahouri <s.sahouri1@gmail.com>
2018-11-30 11:48:13 -0500

    rest template with user agent

--- src/main/java/brainpower/scientist/ScientistController.java
+++ src/main/java/brainpower/scientist/ScientistController.java
@@ -1,5 +1,8 @@
 package brainpower.scientist;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
@@ -10,13 +13,25 @@ import brainpower.scientist.model.ChuckResponse;
 
 @Controller
 public class ScientistController {
+	private RestTemplate restTemplateWithUserAgent;
+	
+	// This is an instance initialization block. It runs when a new instance of the class is created--
+	// right before the constructor.
+	{
+	    // This configures the restTemplateWithUserAgent to include a User-Agent header with every HTTP
+		// request. Some of the APIs in this demo have a bug where User-Agent is required.
+		ClientHttpRequestInterceptor interceptor = (request, body, execution) -> {
+	        request.getHeaders().add(HttpHeaders.USER_AGENT, "Spring");
+	        return execution.execute(request, body);
+	    };
+	    restTemplateWithUserAgent = new RestTemplateBuilder().additionalInterceptors(interceptor).build();
+	}
 
 	@RequestMapping("/")
 	public ModelAndView showIndex() {
 
 		String url = "https://api.chucknorris.io/jokes/random";
-		RestTemplate rest = new RestTemplate();
-		ChuckResponse rep = rest.getForObject(url, ChuckResponse.class);
+		ChuckResponse rep = restTemplateWithUserAgent.getForObject(url, ChuckResponse.class);
 		return new ModelAndView("index", "chuck", rep);
 	}
 
