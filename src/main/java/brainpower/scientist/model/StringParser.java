97d080bfee42ed7cab78d40474ada77035978887
Simeon Kraft <kraft.simeon.sk@gmail.com>
2018-12-06 14:44:34 -0500

    parser is still sub-optimal

--- src/main/java/brainpower/scientist/model/StringParser.java
+++ src/main/java/brainpower/scientist/model/StringParser.java
@@ -5,12 +5,9 @@ public class StringParser {
 	
 	public static String parseString(String joke, String name) {
 		
-		String newString = joke.replaceAll("(Chuck Norris)|(Chuck|Norris)", name)
-				.replaceAll("(?:^|\\W)his(?:$|\\W)", " their ")
-				.replaceAll("(him)", " them ")
-				.replaceAll("((?:^|\\W)he(?:$|\\W))|((?:^|\\\\W)He(?:$|\\\\W))", " they ");
+		String newString = joke.replaceAll("(Chuck Norris)|(Chuck|Norris)", name);
 		
-		
+		//account for pronouns
 		
 		return newString;
 	}
