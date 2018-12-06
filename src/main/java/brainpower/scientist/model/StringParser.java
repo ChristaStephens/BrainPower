0931357476eb2f72df2e2a67dc387bc91c122011
Simeon Kraft <kraft.simeon.sk@gmail.com>
2018-12-06 09:20:30 -0500

    formatted strength result

--- src/main/java/brainpower/scientist/model/StringParser.java
+++ src/main/java/brainpower/scientist/model/StringParser.java
@@ -5,10 +5,10 @@ public class StringParser {
 	
 	public static String parseString(String joke, String name) {
 		
-		String newString = joke.replaceAll("(Chuck Norris)|(Chuck|Norris)", name);
-//				.replaceAll("(?:^|\\W)his(?:$|\\W)", " their ")
-//				.replaceAll("(him)", " them ")
-//				.replaceAll("((?:^|\\W)he(?:$|\\W))|((?:^|\\\\W)He(?:$|\\\\W))", " they ");
+		String newString = joke.replaceAll("(Chuck Norris)|(Chuck|Norris)", name)
+				.replaceAll("(?:^|\\W)his(?:$|\\W)", " their ")
+				.replaceAll("(him)", " them ")
+				.replaceAll("((?:^|\\W)he(?:$|\\W))|((?:^|\\\\W)He(?:$|\\\\W))", " they ");
 		
 		
 		
